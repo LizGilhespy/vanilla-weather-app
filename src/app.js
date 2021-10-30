7d125badee8d1c022968e173c9f900cdff9989bd
LizGilhespy <90154750+LizGilhespy@users.noreply.github.com>
2021-10-30 11:41:28 +0100

    Corrected js to show forecast data

--- src/app.js
+++ src/app.js
@@ -26,7 +26,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}${timeOfDay}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -50,6 +51,14 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "be60748992fab0f5da8162563fb21245";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   celsius = response.data.main.temp;
   let temperatureElement = document.querySelector("#temperature");
@@ -70,9 +79,9 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-}
 
-displayForecast();
+  getForecast(response.data.coord);
+}
 
 function search(city) {
   let apiKey = "be60748992fab0f5da8162563fb21245";
